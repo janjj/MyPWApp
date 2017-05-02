3a430f8e7cfc8c7bc4efbb66c14a649987bc10bb
Jan <privat@jan-jaeger.de>
2017-05-02 21:45:48 +0200

    ADD platform

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {NavController, Platform} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  ready = false;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,public plt: Platform) {
+    plt.ready().then(() => {
+      this.ready = true;
+    });
 
+  }
+
+  refresh(){
+    this.ready = !this.ready;
   }
 
 }
