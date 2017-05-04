4a43595b77602561a2e9d7474f2b5dcedb34d0c3
Jan Jäger <privat@jan-jaeger.de>
2017-05-04 20:47:07 +0200

    Correct gps add provider

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-contact',
@@ -7,8 +8,21 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public geolocation: Geolocation) {
 
+     /* this.geolocation.getCurrentPosition().then((resp) => {
+          // resp.coords.latitude
+          // resp.coords.longitude
+      }).catch((error) => {
+          console.log('Error getting location', error);
+      });
+
+      let watch = this.geolocation.watchPosition();
+      watch.subscribe((data) => {
+          // data can be a set of coordinates, or an error (if an error occurred).
+          // data.coords.latitude
+          // data.coords.longitude
+      }); */
   }
 
 }
