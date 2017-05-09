66e44dc5b011164879dcad4d78b16d20c3ac8dc2
Jan <privat@jan-jaeger.de>
2017-05-09 23:44:45 +0200

    RENAME home->about ADD about text

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,23 +9,8 @@ import {Geolocation} from '@ionic-native/geolocation';
 })
 export class HomePage {
 
-    constructor(public navCtrl: NavController, private geolocation: Geolocation) {
+    constructor(public navCtrl: NavController) {
 
-        /*
-         this.geolocation.getCurrentPosition().then((resp) => {
-         // resp.coords.latitude
-         // resp.coords.longitude
-         }).catch((error) => {
-         console.log('Error getting location', error);
-         });
-
-         let watch = this.geolocation.watchPosition();
-         watch.subscribe((data) => {
-         // data can be a set of coordinates, or an error (if an error occurred).
-         // data.coords.latitude
-         // data.coords.longitude
-         });
-         */
     }
 
 }
